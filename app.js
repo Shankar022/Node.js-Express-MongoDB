be0d7441ad851d5a9d0690302d4b82c29cbe41b4
Shannkar022 <shankarweb22@gmail.com>
2021-11-25 10:19:04 +0530

    Handling POST requests.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/',(req,res) =>{
 //     res.status(200).json({message:'Hello from the sever side...'});
@@ -21,6 +22,21 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
 })
 
 const port = 3000;
